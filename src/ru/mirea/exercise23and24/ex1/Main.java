1ed80b3cc1cac436b25a2e58704fca5495295093
gnom-note <moifoto2014@yandex.ru>
2020-11-27 18:22:54 +0300

    end

---
+++ src/ru/mirea/exercise23and24/ex1/Main.java
@@ -0,0 +1,7 @@
+package ru.mirea.exercise23and24.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        CsvParsing m1 = new CsvParsing();
+    }
+}
---
+++ src/ru/mirea/exercise23and24/ex2/Main.java
@@ -0,0 +1,7 @@
+package ru.mirea.exercise23and24.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        HtmlPars h1 = new HtmlPars();
+    }
+}
